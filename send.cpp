e2b0b75a7a63195eca3186b295f6127b2fcd66a3
ryan <62574361+ryankpa@users.noreply.github.com>
2020-12-03 23:18:18 -0800

    start on rsa encryption
    moved file opening to a function to clean main; altered encryptMessage to save AES key and iv for next step

--- send.cpp
+++ send.cpp
@@ -7,6 +7,7 @@ This program takes in <#> command line arguments:
 	argv[1]	- path to plaintext for encryption
 	...
 */
+#pragma warning(disable: 4996)	// included to let me use deprecated openssl functions
 #include <stdlib.h>
 #include <iostream>
 #include <openssl/rsa.h>
@@ -14,21 +15,35 @@ This program takes in <#> command line arguments:
 #include <openssl/bn.h>
 #include <openssl/evp.h>
 #include <openssl/rand.h>
+#include <openssl/provider.h>
 
 #define countof(array) (sizeof(array) / sizeof(array[0]))
 
 using namespace std;
 
 // function prototypes
-void encryptMessage(FILE* plaintext, FILE* transmitted_msg);
+void fileSetup(FILE* &plaintext, FILE* &transmitted_msg, char* argv[]);
+void encryptMessage(FILE* plaintext, FILE* transmitted_msg, unsigned char* key, unsigned char* IV);
 int AESEncrypt(FILE* in, FILE* out, unsigned char* key, unsigned char* iv);
-// for debugging
-int AESDecrypt(FILE* in, FILE* out, unsigned char* key, unsigned char* iv);
+void encryptAESKey(char* filename, unsigned char* AESkey, unsigned char* iv);
 
 int main(int argc, char* argv[]) {
 	FILE* plaintext, * transmitted_msg;
-	errno_t err;	// for use with fopen_s
+	unsigned char AESkey[16], iv[16];
+	fileSetup(plaintext, transmitted_msg, argv);
+	encryptMessage(plaintext, transmitted_msg, AESkey, iv);
+	cout << "message encryption done\n";
+	encryptAESKey(argv[2], AESkey, iv);
+	// closing files
+	errno_t err = fclose(plaintext);
+	err = fclose(transmitted_msg);
 
+	return 0;
+}
+
+// may add onto this...
+void fileSetup(FILE* &plaintext, FILE* &transmitted_msg, char* argv[]) {
+	errno_t err;	// for use with fopen_s
 	// opening files for input/writing
 	err = fopen_s(&plaintext, argv[1], "r");
 	if (plaintext == NULL) perror("Error opening file");
@@ -41,30 +56,17 @@ int main(int argc, char* argv[]) {
 	strncpy_s(new_filename, countof(new_filename), argv[1] + slash_pos, strlen(argv[1] + slash_pos) - 4);
 	strcat_s(new_filename, countof(new_filename), "_encrypted.txt");
 	err = fopen_s(&transmitted_msg, new_filename, "w+");
-	encryptMessage(plaintext, transmitted_msg);
-	// closing files
-	err = fclose(plaintext);
-	err = fclose(transmitted_msg);
-	/* EVP RSA STUFF
-	make use of EVP functions!
-	EVP_PKEY* pkey;
-	pkey = EVP_PKEY_new();
-	this stuff generates an RSA public key
-	EVP_PKEY_CTX* ctx2 = EVP_PKEY_CTX_new_id(EVP_PKEY_RSA, NULL);
-	EVP_PKEY_keygen_init(ctx2);
-	EVP_PKEY_keygen(ctx2, &pkey);
-	BIO* out = BIO_new_fp(stdout, BIO_NOCLOSE);
-	EVP_PKEY_print_public(out, pkey, 1, NULL);
-	*/
-	return 0;
 }
 
-void encryptMessage(FILE* plaintext, FILE* transmitted_msg) {
+void encryptMessage(FILE* plaintext, FILE* transmitted_msg, unsigned char* key, unsigned char* IV) {
 	// AES keygen
 	unsigned char AESkey[16], iv[16];
 	RAND_bytes(AESkey, sizeof AESkey);
 	RAND_bytes(iv, sizeof iv);
-
+	// storing AES key and iv
+	memcpy(key, AESkey, 16);
+	memcpy(IV, iv, 16);
+	// encrypting
 	AESEncrypt(plaintext, transmitted_msg, AESkey, iv);
 }
 /*
@@ -98,4 +100,24 @@ int AESEncrypt(FILE* in, FILE* out, unsigned char* key, unsigned char* iv) {
 	fwrite(outbuf, 1, outlen, out);
 	EVP_CIPHER_CTX_cleanup(ctx);
 	return 0;
+}
+
+void encryptAESKey(char* filename, unsigned char* AESkey, unsigned char* iv) {
+	unsigned char* encAES, *encIV;
+	// opening pub key
+	FILE* pubkey_file, *test_file;
+	errno_t err = fopen_s(&pubkey_file, filename, "r");
+	err = fopen_s(&test_file, "test.txt", "w+");
+	RSA* rsa;
+	rsa = PEM_read_RSA_PUBKEY(pubkey_file, NULL, 0, NULL);
+	// encrypt AES key
+	encAES = new unsigned char[RSA_size(rsa)];
+	RSA_public_encrypt(RSA_size(rsa), AESkey, encAES, rsa, RSA_NO_PADDING);
+	// encrypt iv
+	// RSA_public_encrypt(RSA_size(rsa), iv, encIV, rsa, RSA_NO_PADDING);
+
+	// FOR DEBUGGING: output encrypted AES and original AES
+	fwrite(encAES, 1, 16, test_file);
+	//fwrite("\n", 1, 1, test_file);
+	//fwrite(encIV, 1, 16, test_file);
 }
