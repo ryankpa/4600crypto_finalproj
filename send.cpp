ad527f0ab499b9788c68ed17005ea772e6af5b87
ryan <62574361+ryankpa@users.noreply.github.com>
2020-12-07 16:28:21 -0800

    RSA encryption added
    Also reduced code for message encryption. Each function now returns the length of the generated ciphertext.

--- send.cpp
+++ send.cpp
@@ -5,17 +5,22 @@ Class: CS 4600 - Crypto and InfoSec
 SEND.CPP
 This program takes in <#> command line arguments:
 	argv[1]	- path to plaintext for encryption
+	argv[2] - path to receiver's public key
 	...
+NOTES/TO-DOS:
+	- message encryption now stores encrypted message in a buffer, also returns length of the encrypted message
+	- RSA encryption works fine. tested with decryption and it was able to correctly decipher AESkey and iv
+	- make sure to store every encryption result in a c string
+		- this is so we can append everything together at the end and write to output file only one time
+	- keep track of size for each ciphertext/signature for writing at beginning of output file
 */
 #pragma warning(disable: 4996)	// included to let me use deprecated openssl functions
 #include <stdlib.h>
 #include <iostream>
 #include <openssl/rsa.h>
 #include <openssl/pem.h>
-#include <openssl/bn.h>
 #include <openssl/evp.h>
 #include <openssl/rand.h>
-#include <openssl/provider.h>
 
 #define countof(array) (sizeof(array) / sizeof(array[0]))
 
@@ -23,17 +28,23 @@ using namespace std;
 
 // function prototypes
 void fileSetup(FILE* &plaintext, FILE* &transmitted_msg, char* argv[]);
-void encryptMessage(FILE* plaintext, FILE* transmitted_msg, unsigned char* key, unsigned char* IV);
-int AESEncrypt(FILE* in, FILE* out, unsigned char* key, unsigned char* iv);
-void encryptAESKey(char* filename, unsigned char* AESkey, unsigned char* iv);
+int encryptMessage(FILE* plaintext, unsigned char* key, unsigned char* iv, unsigned char* ciphertxt);
+int encryptAESKey(char* filename, unsigned char* AESkey, unsigned char* iv, unsigned char* encAES, unsigned char* encIV);
 
 int main(int argc, char* argv[]) {
 	FILE* plaintext, * transmitted_msg;
 	unsigned char AESkey[16], iv[16];
+	unsigned char encryptedMsg[1024 + EVP_MAX_BLOCK_LENGTH];
+	int encrypt_msg_len, encrypted_keys_len;
+	cout << "Opening files...\n";
 	fileSetup(plaintext, transmitted_msg, argv);
-	encryptMessage(plaintext, transmitted_msg, AESkey, iv);
-	cout << "message encryption done\n";
-	encryptAESKey(argv[2], AESkey, iv);
+	cout << "Files opened. Encrypting message...\n";
+	encrypt_msg_len = encryptMessage(plaintext, AESkey, iv, encryptedMsg);
+	cout << "Message encryption done. Encrypting key...\n";
+	unsigned char encAES[256], encIV[256];
+	encrypted_keys_len = encryptAESKey(argv[2], AESkey, iv, encAES, encIV);
+	cout << "Key encryption done. Generating MAC...\n";
+	// fwrite(encryptedMsg, 1, encrypt_len, transmitted_msg);
 	// closing files
 	errno_t err = fclose(plaintext);
 	err = fclose(transmitted_msg);
@@ -58,66 +69,54 @@ void fileSetup(FILE* &plaintext, FILE* &transmitted_msg, char* argv[]) {
 	err = fopen_s(&transmitted_msg, new_filename, "w+");
 }
 
-void encryptMessage(FILE* plaintext, FILE* transmitted_msg, unsigned char* key, unsigned char* IV) {
-	// AES keygen
-	unsigned char AESkey[16], iv[16];
-	RAND_bytes(AESkey, sizeof AESkey);
-	RAND_bytes(iv, sizeof iv);
-	// storing AES key and iv
-	memcpy(key, AESkey, 16);
-	memcpy(IV, iv, 16);
-	// encrypting
-	AESEncrypt(plaintext, transmitted_msg, AESkey, iv);
-}
 /*
-AESEncryption():
-	- borrows from openssl.org's encryption function example
+encryptMessage()
+	- borrows from OpenSSL's encryption/decryption example
 	  which can be found here: https://www.openssl.org/docs/man1.0.2/man3/EVP_EncryptInit.html
 */
-int AESEncrypt(FILE* in, FILE* out, unsigned char* key, unsigned char* iv) {
-	/*setting up for AES encryption*/
-	int inlen, outlen;
-	unsigned char inbuf[1024], outbuf[1024 + EVP_MAX_BLOCK_LENGTH];
+int encryptMessage(FILE* plaintext, unsigned char* key, unsigned char* iv, unsigned char* ciphertxt) {
+	// AES keygen
+	RAND_bytes(key, sizeof key);
+	RAND_bytes(iv, sizeof iv);
+	// encrypting
+	unsigned char inbuf[1024];
+	int inlen, outlen = 0;
+	int final_ciphertxt_len = 0;
 	EVP_CIPHER_CTX* ctx = EVP_CIPHER_CTX_new();
 	EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv);
-
-	/*encrypting*/
 	for (;;) {
-		inlen = fread(inbuf, 1, 1024, in);
+		inlen = fread(inbuf, 1, 1024, plaintext);
 		if (inlen <= 0) break;
-		if (!EVP_CipherUpdate(ctx, outbuf, &outlen, inbuf, inlen)) {
-			// cleanup cipher context due to error
-			EVP_CIPHER_CTX_cleanup(ctx);
-			return 0;
-		}
-		fwrite(outbuf, 1, outlen, out);
+		EVP_EncryptUpdate(ctx, ciphertxt + outlen, &outlen, inbuf, inlen);
+		final_ciphertxt_len += outlen;
 	}
-	if (!EVP_CipherFinal_ex(ctx, outbuf, &outlen)) {
-		// cleanup cipher context due to error
-		EVP_CIPHER_CTX_cleanup(ctx);
-		return 0;
-	}
-	fwrite(outbuf, 1, outlen, out);
-	EVP_CIPHER_CTX_cleanup(ctx);
-	return 0;
+	EVP_EncryptFinal_ex(ctx, ciphertxt + outlen, &outlen);
+	final_ciphertxt_len += outlen;
+	EVP_CIPHER_CTX_free(ctx);
+	return final_ciphertxt_len;
 }
 
-void encryptAESKey(char* filename, unsigned char* AESkey, unsigned char* iv) {
-	unsigned char* encAES, *encIV;
+/*
+encryptAESKey():
+	- uses RSA encryption to encrypt the AES key and iv used to generate ciphertext
+	- returns length of the two encrypted texts
+	- reference used: https://shanetully.com/2012/04/simple-public-key-encryption-with-rsa-and-openssl/
+*/
+int encryptAESKey(char* filename, unsigned char* AESkey, unsigned char* iv, unsigned char* encAES, unsigned char* encIV) {
+	int encAES_len, encIV_len;
 	// opening pub key
 	FILE* pubkey_file, *test_file;
 	errno_t err = fopen_s(&pubkey_file, filename, "r");
 	err = fopen_s(&test_file, "test.txt", "w+");
 	RSA* rsa;
 	rsa = PEM_read_RSA_PUBKEY(pubkey_file, NULL, 0, NULL);
-	// encrypt AES key
-	encAES = new unsigned char[RSA_size(rsa)];
-	RSA_public_encrypt(RSA_size(rsa), AESkey, encAES, rsa, RSA_NO_PADDING);
+	// encrypting AES key
+	encAES_len = RSA_public_encrypt(RSA_size(rsa), AESkey, encAES, rsa, RSA_NO_PADDING);
 	// encrypt iv
-	// RSA_public_encrypt(RSA_size(rsa), iv, encIV, rsa, RSA_NO_PADDING);
-
+	encIV_len = RSA_public_encrypt(RSA_size(rsa), iv, encIV, rsa, RSA_NO_PADDING);
 	// FOR DEBUGGING: output encrypted AES and original AES
-	fwrite(encAES, 1, 16, test_file);
-	//fwrite("\n", 1, 1, test_file);
-	//fwrite(encIV, 1, 16, test_file);
+	fwrite(encAES, sizeof(*encAES), RSA_size(rsa), test_file);
+	fwrite("\n", sizeof("\n"), 1, test_file);
+	fwrite(encIV, sizeof(*encIV), RSA_size(rsa), test_file);
+	return (encAES_len + encIV_len);
 }
